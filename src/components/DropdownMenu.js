bfebf382cb2e7ba723684ab7897af7b5acc67023
ruffy <raphi154@gmail.com>
2025-02-25 01:49:35 +0100

    dashboard v2

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -7,44 +7,39 @@ const DropdownMenu = React.forwardRef(({ openLoginModal, openDepositModal, openW
                 {isLoggedIn ? (
                     <>
                         <li onClick={openDepositModal}>
-                            <svg width="16" height="16" fill="currentColor" className="bi bi-cash" viewBox="0 0 16 16">
-                                <path d="M8.5 0a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5zM8 2a6 6 0 1 0 0 12 6 6 0 0 0 0-12zM8 1a5 5 0 1 1 0 10A5 5 0 0 1 8 1z"/>
-                                <path d="M8 4a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM8 7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V7.5A.5.5 0 0 1 8 7zM8 10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 8 10z"/>
+                            <svg className="icon" viewBox="0 0 16 16">
+                                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm.5 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1H9v3a.5.5 0 0 1-1 0v-3H5a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 .5-.5z"/>
                             </svg>
                             Deposit
                         </li>
                         <li onClick={openWithdrawModal}>
-                            <svg width="16" height="16" fill="currentColor" className="bi bi-arrow-down-circle" viewBox="0 0 16 16">
-                                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm0 1a7 7 0 1 1 0 14A7 7 0 0 1 8 1z"/>
-                                <path fillRule="evenodd" d="M8.5 4a.5.5 0 0 1 .5.5v3.793l2.146-2.147a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 0 1 .708-.708L8 8.293V4.5a.5.5 0 0 1 .5-.5z"/>
+                            <svg className="icon" viewBox="0 0  16 16">
+                                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.354 8.354a.5.5 0 0 1-.708.708L8.5 7.707V12a.5.5 0 0 1-1 0V7.707L5.354 9.062a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3z"/>
                             </svg>
                             Withdraw
                         </li>
                         <li onClick={openReferralDashboard}>
-                            <svg width="16" height="16" fill="currentColor" className="bi bi-gift" viewBox="0 0 16 16">
-                                <path d="M4 3.5a1.5 1.5 0 1 1 3 0c0 .5-.5 1-1 1H4a1 1 0 0 1-1-1z"/>
-                                <path d="M10 3.5a1.5 1.5 0 1 1 3 0c0 .5-.5 1-1 1h-2a1 1 0 0 1-1-1z"/>
-                                <path d="M4 4h8a1 1 0 0 1 1 1v2H3V5a1 1 0 0 1 1-1zm-2 3h12v6a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7z"/>
+                            <svg className="icon" viewBox="0 0 16 16">
+                                <path d="M7 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H4s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                             </svg>
                             Referral Program
                         </li>
                         <li onClick={openInfoModal}>
-                            <svg width="16" height="16" fill="currentColor" className="bi bi-info-circle" viewBox="0 0 16 16">
-                                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm0 1a7 7 0 1 1 0 14A7 7 0 0 1 8 1z"/>
-                                <path d="M8 4a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM8 7a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V7.5A.5.5 0 0 1 8 7z"/>
+                            <svg className="icon" viewBox="0 0 16 16">
+                                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm.5 7.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 1 0v2.5zm0 3a.5.5 0 0 1-1 0V8a.5.5 0 0 1 1 0v2.5z"/>
                             </svg>
                             Info
                         </li>
-                        <li onClick={handleLogout}>
-                            <svg width="16" height="16" fill="currentColor" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M10.5 0a.5.5 0 0 1 .5.5v4.5h-1V1H1v14h10v-3h1v4.5a.5.5 0 0 1-.5.5H1a.5.5 0 0 1-.5-.5V.5A.5.5 0 0 1 1 0h9.5zM15 8a.5.5 0 0 1 0 1H5.707l2.146 2.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708.708L5.707 7H15z"/>
+                        <li onClick={handleLogout} className="logout">
+                            <svg className="icon" viewBox="0 0 16 16">
+                                <path d="M10.5 0a.5.5 0 0 1 .5.5v4.5h-1V1H1v14h10v-3h1v4.5a.5.5 0 0 1-.5.5H1a.5.5 0 0 1-.5-.5V.5A.5.5 0 0 1 1 0h9.5zM15 8a.5.5 0 0 1 0 1H5.707l2.146 2.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708.708L5.707 7H15z"/>
                             </svg>
                             Logout
                         </li>
                     </>
                 ) : (
                     <li onClick={openLoginModal}>
-                        <svg width="16" height="16" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
+                        <svg className="icon" viewBox="0 0 16 16">
                             <path d="M3 14s-1 0-1-1 1-1 1-1h10s1 0 1 1-1 1-1 1H3zm8-9a3 3 0 1 0-6 0 3 3 0 0 0 6 0z"/>
                         </svg>
                         Login
--- src/components/ReferralDashboard.js
+++ src/components/ReferralDashboard.js
@@ -8,6 +8,8 @@ const ReferralDashboard = ({ closeModal }) => {
         totalRewards: 0,
         gamificationPoints: 0
     });
+    const [referralLink, setReferralLink] = useState(""); // Referral-Link
+    const [copySuccess, setCopySuccess] = useState(false); // Erfolgsmeldung
 
     useEffect(() => {
         const fetchData = async () => {
@@ -26,6 +28,7 @@ const ReferralDashboard = ({ closeModal }) => {
                     totalRewards: data.totalRewards || 0,
                     gamificationPoints: data.gamificationPoints || 0
                 });
+                setReferralLink(`https://hustlerapp.com/signup?ref=${data.referralCode}`);
             } catch (error) {
                 console.error("Error fetching referral data:", error);
             }
@@ -33,10 +36,17 @@ const ReferralDashboard = ({ closeModal }) => {
         fetchData();
     }, []);
 
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(referralLink).then(() => {
+            setCopySuccess(true);
+            setTimeout(() => setCopySuccess(false), 2000); // Erfolgsmeldung nach 2s ausblenden
+        });
+    };
+
     return (
         <div className="modal-backdrop" onClick={closeModal}>
             <div className="modal referral-dashboard" onClick={(e) => e.stopPropagation()}>
-                <div className="referral-title">Referrals</div>
+                <div className="referral-title">Referral dash</div>
                 <p className="total-registrations">
                     {referralStats.totalReferrals} <span>registrations</span>
                 </p>
@@ -72,13 +82,22 @@ const ReferralDashboard = ({ closeModal }) => {
                 </div>
 
                 <div className="progress-container">
-                    <p>Total Gamification Points</p>
+                    <p>Total Points</p>
                     <div className="progress-wrapper">
                         <div className="progress-bar">
                             <div className="progress-fill" style={{ width: `${Math.min(referralStats.gamificationPoints / 1000, 1) * 100}%` }}></div>
                         </div>
                         <span className="progress-value">{referralStats.gamificationPoints}</span>
                     </div>
+                </div>
+
+                {/* Referral Link Section (100% linksbündig) */}
+                <div className="referral-link-container">
+                    <p>Your Referral Link</p>
+                    <div className="referral-link" onClick={copyToClipboard}>
+                        {referralLink}
+                    </div>
+                    {copySuccess && <p className="copy-success">Copied to clipboard!</p>}
                 </div>
             </div>
         </div>
